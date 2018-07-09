09d7fff7099cd85446032af39139586b67cdf94c
Roberto Achar <robertoachar@gmail.com>
2018-07-09 16:57:08 -0300

    feature: add generator

---
+++ generators/app/index.js
@@ -0,0 +1,80 @@
+const Generator = require('yeoman-generator');
+
+const files = require('./files');
+
+module.exports = class extends Generator {
+  initializing() {
+    this.log('Statik Project Generator');
+    this.log('Made with ❤️  by Statik');
+    this.log();
+  }
+
+  prompting() {
+    const done = this.async();
+
+    const prompts = [
+      {
+        type: 'input',
+        name: 'project',
+        message: 'What is the name of your project?',
+        default: 'statik-project'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of your project?',
+        default: 'A statik project'
+      }
+    ];
+
+    this.prompt(prompts).then((props) => {
+      this.props = props;
+
+      console.log(props);
+      done();
+    });
+  }
+
+  writing() {
+    this.log();
+    this.log('🚀  Generating project...');
+    this.log();
+
+    const { project, description } = this.props;
+
+    const templates = {
+      project,
+      description,
+      year: new Date().getFullYear()
+    };
+
+    files.forEach((file) => {
+      this.fs.copyTpl(
+        this.templatePath(file.template),
+        this.destinationPath(file.destination),
+        templates
+      );
+    });
+  }
+
+  install() {
+    this.log();
+    this.log('📦  Installing dependencies...');
+    this.log();
+
+    this.npmInstall(
+      [
+        'eslint',
+        'eslint-config-prettier',
+        'eslint-plugin-prettier',
+        'prettier'
+      ],
+      { 'save-dev': true }
+    );
+  }
+
+  end() {
+    this.log();
+    this.log('🎉  Successfully generated!');
+  }
+};
